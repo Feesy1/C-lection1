3089fbc4b8e18fbd8f86462ab5119ab2063c08bc
Feesy <peter1337228@gmail.com>
2023-02-13 21:19:16 +0300

    Add Random

--- Example2_sum/Program.cs
+++ Example2_sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int number1 = 311;
-int number2 = 590;
+﻿int number1 = new Random(). Next(1,20); //  1 2 3 4 .... 19 
+Console.WriteLine(number1);
+int number2 = new Random(). Next(1,20);
+Console.WriteLine(number2);
 int result = number1 + number2;
 Console.WriteLine(result);
